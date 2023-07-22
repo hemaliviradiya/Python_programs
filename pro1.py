1eec896023f8bb3c386157e729988da80beeff74
Hemali_Viradiya14 <113543974+hemaliviradiya@users.noreply.github.com>
2023-07-22 13:54:01 +0530

    Update pro1.py

--- pro1.py
+++ pro1.py
@@ -1,6 +1,9 @@
 mylist=[[1,2,3],["ram","manish","bharat"],[1.5,1.2,3.5]]
-mylist.append("Ravi ")
-mylist.insert(0,100)
-list1=mylist
-mylist.remove(100)
+#mylist.append("Ravi ")
+#mylist.insert(0,100)
+#list1=mylist
+#mylist.remove(100)
 print(list1)
+
+O/P :=
+[[1, 2, 3], ['ram', 'manish', 'bharat'], [1.5, 1.2, 3.5], 'Ravi ']
