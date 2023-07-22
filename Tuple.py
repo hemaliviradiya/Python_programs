f7e48172ee3044a874f8c57938aaec9196f329c0
Hemali_Viradiya14 <113543974+hemaliviradiya@users.noreply.github.com>
2023-07-22 13:50:01 +0530

    Update Tuple.py

--- Tuple.py
+++ Tuple.py
@@ -3,3 +3,15 @@ tuple1 = (1,2,5.0,"Hello")
 #******for run append write type(mylist)in commnad line***********#
 tuple.append(100)
 print(tuple1[-1])
+
+O/P :-
+type(mylist)
+list
+
+----------------------------------------------------------
+mylist = [10,20,30,40,50,60,70,80,90]
+l1=sorted(mylist)
+print(l1)
+
+O/P :-
+[10, 20, 30, 40, 50, 60, 70, 80, 90]
