84204a4815233e3da62e57bb3d04c21f06b99078
Hemali_Viradiya14 <113543974+hemaliviradiya@users.noreply.github.com>
2023-07-25 13:57:24 +0530

    Update Opps_Python.py

--- Opps_Python.py
+++ Opps_Python.py
@@ -1,1 +1,22 @@
+---------------------------create class-------------------------------
+# 1st method
+class abc :
+    def show():
+        print("My firt python class....")
+        
+abc.show()
+
+#2nd method //using constructor
+class abc :
+    def show(self):
+        print("My firt python class....")
+        
+obj=abc()
+obj.show()
+
+
+O/P :-
+My firt python class....
+
+---------------------------create class-------------------------------
 
