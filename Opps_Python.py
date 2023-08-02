c7bcab4299c4e9f00aaac44ff1cd4c9c07951c52
Hemali_Viradiya14 <113543974+hemaliviradiya@users.noreply.github.com>
2023-08-02 11:49:52 +0530

    Update Opps_Python.py

--- Opps_Python.py
+++ Opps_Python.py
@@ -68,5 +68,65 @@ obj1.show()
 O/P :-
 xyz class method..
 
----------------------------function override method-------------------------------
+---------------------------any type object-------------------------------
 
+class abc :
+    
+    def show(self):
+        print("Welcome..")
+        
+class xyz(abc):
+    def show(self):
+        print("xyz class method..")
+        
+#obj=abc("Hemali")
+        
+obj=any
+        
+obj1=abc()
+obj=obj1
+obj.show()
+
+obj2=xyz()
+obj=obj2
+obj.show()
+
+O/P :-
+Welcome..
+xyz class method..
+
+---------------------------if condition-------------------------------
+
+class abc :
+    
+    def show(self):
+        print("Welcome..")
+        
+class xyz(abc):
+    def show(self):
+        print("xyz class method..")
+        
+#obj=abc("Hemali")
+        
+obj=any
+a=int(input("Enter a value 0 or 1:"))
+
+if(a==0):
+        
+    obj1=abc()
+    obj=obj1
+    obj.show()
+if(a==1):   
+
+    obj2=xyz()
+    obj=obj2
+    obj.show()
+
+O/P :-
+1] Enter a value 0 or 1:1
+xyz class method..
+
+2] Enter a value 0 or 1:0
+Welcome..
+
+---------------------------if condition-------------------------------
