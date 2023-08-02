804ed3e999f5941f3320bb093ea3cae474f83745
Hemali_Viradiya14 <113543974+hemaliviradiya@users.noreply.github.com>
2023-08-02 12:09:21 +0530

    Update Numpy.py

--- Numpy/Numpy.py
+++ Numpy/Numpy.py
@@ -1,1 +1,34 @@
 
+-------------------------Numpy 1D array print------------------------
+
+import numpy as np
+
+n1=np.array([10,20,30,40,50])
+print(n1)
+
+O/P :-
+[10 20 30 40 50]
+
+-------------------------Numpy 2D array print------------------------
+
+import numpy as np
+
+n1=np.array([[10,20,30,40,50],[5,10,15,20,25]])
+print(n1)
+
+O/P :-
+[[10 20 30 40 50]
+ [ 5 10 15 20 25]]
+
+-------------------------Numpy shape of 2D array print------------------------
+
+import numpy as np
+
+n1=np.array([[10,20,30,40,50],[5,10,15,20,25]])
+print(n1.shape)
+
+O/P :-
+(2, 5)
+
+-------------------------Numpy shape of 2D array print------------------------
+
