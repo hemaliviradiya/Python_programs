32f5f3bff0ef308c3d0ec15c770502043f6f22c9
Hemali_Viradiya14 <113543974+hemaliviradiya@users.noreply.github.com>
2023-08-02 12:28:54 +0530

    Update Numpy.py

--- Numpy/Numpy.py
+++ Numpy/Numpy.py
@@ -30,5 +30,80 @@ print(n1.shape)
 O/P :-
 (2, 5)
 
--------------------------Numpy shape of 2D array print------------------------
+-------------------------Numpy zeros matrix------------------------
+import numpy as np
+
+n1=np.zeros((2,3))
+print(n1)
+
+O/P :-
+[[0. 0. 0.]
+ [0. 0. 0.]]
+
+-------------------------Numpy ones matrix------------------------
+
+import numpy as np
+
+n1=np.ones((3,4))
+print(n1)
+
+O/P :-
+[[1. 1. 1. 1.]
+ [1. 1. 1. 1.]
+ [1. 1. 1. 1.]]
+
+-------------------------Numpy print range of number------------------------
+import numpy as np
+
+n1=np.arange(11,20)
+print(n1)
+
+O/P :-
+[11 12 13 14 15 16 17 18 19]
+
+-------------------------Numpy print even value range of number------------------------
+
+import numpy as np
+
+n1=np.arange(11,20,2)
+print(n1)
+
+O/P :-
+[11 13 15 17 19]
+
+-------------------------Numpy print gape of 10 value range of number------------------------
+import numpy as np
+
+n1=np.arange(10,100,10)
+print(n1)
+
+O/P :-
+[10 20 30 40 50 60 70 80 90]
+
+-------------------------Numpy print 3*3 matrix------------------------
+import numpy as np
+
+n1=np.arange(10,100,10)
+n1.shape=(3,3)
+print(n1)
+
+O/P :-
+[[10 20 30]
+ [40 50 60]
+ [70 80 90]]
+
+-------------------------Numpy print 4*2 matrix------------------------
+import numpy as np
+
+n1=np.arange(10,90,10)
+n1.shape=(4,2)
+print(n1)
+
+O/P :-
+[[10 20]
+ [30 40]
+ [50 60]
+ [70 80]]
+
+-------------------------Numpy print 4*2 matrix------------------------
 
